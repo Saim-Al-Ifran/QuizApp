4fd56652efc1f187d25b485e6c8fecfb4f46f074
Saim <admin@gmail.com>
2022-12-12 15:13:05 +0600

    initial commit

---
+++ question.js
@@ -0,0 +1,64 @@
+let Questions = [
+    //question 1
+    {
+        numb:1,
+        question:"What Does HTML Stand For?",
+        answer:"Hyper Text Markup Language",
+        options:[
+            "Hyper Text Multi Language",
+            "Hyper Text Multipule Language",
+            "Hyper Text Markup Language",
+            "Hyper Text Makeup Language"
+        ]
+    },
+ //question 2
+    {
+        numb:2,
+        question:"What Does CSS Stand For?",
+        answer:"Cascading style Sheet",
+        options:[
+            "Common Style Sheet",
+            "Colorfull style Sheet",
+            "Cascading style Sheet",
+            "Computer style Sheet"
+        ]
+    },
+     //question 3
+
+     {
+        numb:3,
+        question:"What Does PHP Stand For?",
+        answer:"HyperText Preprocessor",
+        options:[
+            "HyperText Preprocessor",
+            "HyperText Programing",
+            "HomeText Preprocessor",
+            "HyperText PrePrograming"
+        ]
+    },
+    //question 4
+    {
+        numb:4,
+        question:"What Does SQL Stand For?",
+        answer:"Structured Query Language",
+        options:[
+            "Structured Query Language",
+            "Structured Question Language",
+            "Structured Query Length",
+            "Stylesheet Query Language"
+        ]
+    },
+   // question 5,
+   {
+    numb:5,
+    question:"What Does XML Stand For?",
+    answer:"Extensible Markup Language",
+    options:[
+        "Extensible Markup Language",
+        "Expendable Markup Language",
+        "Expensive Markup Language",
+        "Extensible Makeup Language"
+    ]
+}
+    
+]
