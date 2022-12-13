ef97d96bcf86ce576e7fd3f427b17488f4864960
Saim <admin@gmail.com>
2022-12-13 11:54:50 +0600

    sdfsa

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const rulesBox = document.querySelector(".RulesBox");
 const exitButton = document.querySelector(".ExitButton");
 const continueButton = document.querySelector(".continueButton");
 const questions = document.querySelector(".Questions");
+const nextBtn = document.querySelector(".nextBtn");
+
+let count = 0;
 
 
 startQuiz.addEventListener("click",function(){
@@ -23,29 +26,54 @@ exitButton.addEventListener("click",function(){
 continueButton.addEventListener("click",function(){
      rulesBox.classList.remove("activeInfo");
      questions.classList.add("activeQuiz");
+     nextBtn.style.display = "block";
+
      showQuestions(0);
 });
 
+
+nextBtn.addEventListener("click",function(){
+           count += 1;
+         
+           if(count < 6){
+               showQuestions(count);
+           }else{
+               alert("you have completed your exam")
+           }
+});
 
 const showQuestions = (index)=>{
 
      let mainQuestion = document.querySelector(".text");
      let quizOptions = document.querySelector(".MyOptions");
      let totalQue =  document.querySelector(".total_que");
+     
 
      mainQuestion.innerHTML = `<span>${Questions[index].numb}. ${Questions[index].question}</span>`;
- 
- 
+
      
       let optionTag  =  `<div class="options">${Questions[index].options[0]}</div> 
                         <div class="options">${Questions[index].options[1]}</div>
                         <div class="options">${Questions[index].options[2]}</div>
                         <div class="options">${Questions[index].options[3]}</div>`;
 
+      let totalQueTag = `<p>${Questions[index].numb} of ${Questions.length} Questions</p>`;
+      totalQue.innerHTML = totalQueTag;              
+                                
+     quizOptions.innerHTML = optionTag;
 
+     const selectOptions = document.querySelectorAll(".options");
 
-                                      
-       quizOptions.innerHTML = optionTag;
+       for(let i = 0; i < selectOptions.length; i++){
+                selectOptions[i].addEventListener("click",function(){
+                           if(this.innerText == Questions[index].answer){
+                                alert("correct answer");
+                           }else{
+                                alert("wrong answer");
+                           }
+                })
+       }
+
 
 
 }
